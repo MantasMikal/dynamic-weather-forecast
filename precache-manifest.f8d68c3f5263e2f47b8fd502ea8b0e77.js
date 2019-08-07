f7304fc23f3efd6a08d50bd89f0bd7157e25a692
MantasMikal <mantas@Mantass-MacBook-Air.local>
2019-08-07 12:10:20 +0100

    Updates

---
+++ precache-manifest.f8d68c3f5263e2f47b8fd502ea8b0e77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c893b7f39c0e4c73c447c59282b333",
+    "url": "/dynamic-weather-forecast/index.html"
+  },
+  {
+    "revision": "8821dfc6977a6a019b2a",
+    "url": "/dynamic-weather-forecast/static/css/main.485ccd36.chunk.css"
+  },
+  {
+    "revision": "9124168c83d46150d09f",
+    "url": "/dynamic-weather-forecast/static/js/2.673aa883.chunk.js"
+  },
+  {
+    "revision": "8821dfc6977a6a019b2a",
+    "url": "/dynamic-weather-forecast/static/js/main.e58ef5f5.chunk.js"
+  },
+  {
+    "revision": "18a6a597723f65da3450",
+    "url": "/dynamic-weather-forecast/static/js/runtime~main.f18c512f.js"
+  },
+  {
+    "revision": "0f496981d22f85def4ba471780ceaae7",
+    "url": "/dynamic-weather-forecast/static/media/cloud1.0f496981.svg"
+  },
+  {
+    "revision": "d620a9e5ff23f58620ee41f35c6e7f33",
+    "url": "/dynamic-weather-forecast/static/media/cloud2.d620a9e5.svg"
+  },
+  {
+    "revision": "96c8ac286f41cb97cedd12ea98fdff74",
+    "url": "/dynamic-weather-forecast/static/media/cloud3.96c8ac28.svg"
+  }
+]);
